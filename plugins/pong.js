47c737bea47e7a78a6753e5fc2ec038867b6e87f
Nathaniel Smith <nate.smith@coxinc.com>
2011-09-25 20:01:17 -0400

    oops, didn't mean to pull this in

--- plugins/pong.js
+++
@@ -1,7 +0,0 @@
-// reply to server PING. This is pretty much a required plugin if you want to
-// stay online.
-exports.plugin = function() {
-	this.onBot('PING', function(message) {
-		this.send('PONG', 'localhost');
-	});
-};
